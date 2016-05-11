56dc0d3c3b768203e527c655ff8a0031b13a6cad
Ben Foxall <ben@pusher.com>
2016-05-11 18:15:15 +0100

    Switch to just a json endpoint in the app

--- app.js
+++ app.js
@@ -1,47 +1,20 @@
 require('dotenv').config()
 
-const fs = require('fs')
-const Mustache = require('mustache')
 const express = require('express')
 const app = express()
+
 const Redis = require('ioredis')
 const redis = new Redis(process.env.REDIS_URL)
 
-
 app.use(express.static('public'))
 
-const tweets = () =>
-  redis
-    .lrange('tweets', 0, -1)
-    .then( result => result.map(JSON.parse) )
-
 app.get('/json', (req, res, next) =>
-  tweets()
-    .then(res.send.bind(res))
-    .catch(next)
+  redis
+    .lrange( 'tweets', 0, -1 )
+    .then( result => result.map( JSON.parse ) )
+    .then( tweets => res.send( tweets ) )
+    .catch( next )
 )
-
-app.get('/csv', (req, res, next) => {
-  res.set('Content-Type', 'text/csv')
-  tweets()
-    .then(tweets => {
-      res.send(
-        csv(['id_str','screen_name','text','name','img'], tweets)
-      )
-    })
-    .catch(next)
-})
-
-
-const tmpl = fs.readFileSync('./public/index.tmpl.html').toString()
-app.get('/', (req, res, next) => {
-  tweets()
-    .then(tweets =>
-      res.send(Mustache.render(tmpl, {tweets: tweets}))
-    )
-    .catch(next)
-})
-
 
 app.get('/config', (req, res) => {
   res.send({
@@ -51,15 +24,3 @@ app.get('/config', (req, res) => {
 })
 
 app.listen(process.env.PORT || 3000)
-
-
-const csv_row = _ =>
-  _.map(i =>
-    i.toString()
-    .replace(/["\n]/g,'')
-  ).join(',') + '\n'
-
-const csv = (cols, data) =>
-  csv_row(cols) + data.reduce(
-    (csv, row) => csv + csv_row(cols.map( c => row[c] ))
-  )
